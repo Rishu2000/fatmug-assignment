fde79969fe74cfd678da0855fcafae34ba87d8ef
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-14 12:11:33 +0530

    Implemented react-router-dom and added switching between pages functionality.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './App.css';
+import Register from './forms/Register'
+import Login from './forms/Login'
 
 function App() {
   return (
-    <div className="App">
-      Rishav
-    </div>
+  <Router>
+    <Switch>
+      <Route path="/register">
+        <Register/>
+      </Route>
+      <Route path="/login">
+        <Login/>
+      </Route>
+      <Route path="/">
+        <h1>Home</h1>
+        <button style={{fontSize:35,padding:"0 10px", margin:"0 10px"}}>
+          <Link to="/login" style={{textDecoration: "none"}}>
+            Login
+          </Link>
+        </button>
+        <button style={{fontSize:35,padding:"0 10px", margin:"0 10px"}}>
+          <Link to="/register" style={{textDecoration:"none"}}>
+            Register
+          </Link>
+        </button>
+      </Route>
+    </Switch>
+  </Router>
   );
 }
 
