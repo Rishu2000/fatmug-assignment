b5276608154b191c05440032a61d5aca3314d8a9
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-14 20:12:45 +0530

    Boiler plate for the server side is ready.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-console.log("server file")
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3001;
+
+app.use(cors());
+
+app.get('/', (req,res) => {
+    res.json("server is on")
+})
+
+app.listen(port,() => {
+    console.log("server is listening to the port "+port);
+})
