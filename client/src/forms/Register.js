28230458f0fc73bb5b4735ebdb144889618a155a
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-15 11:42:22 +0530

    Implemented knex and pg functionality for inserting data into database.

--- client/src/forms/Register.js
+++ client/src/forms/Register.js
@@ -1,22 +1,71 @@
-import React from 'react'
-import FormInput from './FormInput'
+import React, {useState} from 'react'
+import axios from 'axios'
 import {Link} from 'react-router-dom'
 
 const Register = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPass, setConfirmPass] = useState('')
+
+
+    let data = JSON.stringify({
+        "name": name,
+        "password": password,
+        "email": email
+    });
+    
+    let config = {
+        method:'POST',
+        url:'http://localhost:3001/',
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+    const handleRegister = () => {
+        console.log(`State { name = ${name}, password = ${password}, email = ${email}, confirmPass = ${confirmPass}}`);
+        if(password === confirmPass) {
+            axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+        }
+        else{
+            console.log("Please enter correct password")
+        }
+    }
+
     return (
         <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor:"gray"}}>
             <div style={{padding:"20px 40px", backgroundColor: 'white', borderRadius:5}}>
                 <Link to="/login" style={{textDecoration: "none"}}>Already a member</Link>
                 <h1 style={{textAlign: 'center', marginBottom:40}}>Register</h1>
-                <FormInput formType="email"/>
-                <FormInput formType="password"/>
-                <FormInput formType="password" textInput="Confirm password"/>
+                <form>
+                    <div className="mb-4" style={{width:"500px"}}>
+                        <input type="text" className="form-control mb-4" placeholder="Name" value={name}
+                            onChange={(e) => setName(e.target.value)}/>
+                        <input type="email" className="form-control mb-4" placeholder="Email" value={email}
+                            onChange={(e) => setEmail(e.target.value)}/>
+                        <input type="password" className="form-control mb-4" placeholder="Password" value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>
+                        <input type="password" className="form-control mb-4" placeholder="Confirm Password" value={confirmPass}
+                            onChange={(e) => setConfirmPass(e.target.value)}/> 
+                    </div>
+                </form>
                 <div style={{display: 'flex', justifyContent:'space-between'}}>
                     <h6></h6>
                     <h6>Not a member yet</h6>
                 </div>
                 <div style={{margin:"30px 0 50px 0", textAlign:"center"}}>
-                    <button style={{textDecoration:"none", width:"150px", backgroundColor:"grey", color:"white", border:"none", borderRadius:"2px", height:45}}>Register</button>
+                    <button style={{textDecoration:"none", width:"150px", backgroundColor:"grey", color:"white", border:"none", borderRadius:"2px", height:45}}
+                        onClick={handleRegister}
+                    >Register
+                    </button>
                 </div>
             </div>
         </div>
--- server/server.js
+++ server/server.js
@@ -4,9 +4,22 @@ const cors = require('cors');
 const port = 3001;
 
 app.use(cors());
+app.use(express.json());
 
+const knex = require('knex')({
+    client:'pg',
+    connection:'postgres://postgres:rishavpgsql@localhost:5432/example_db'
+});
 app.get('/', (req,res) => {
     res.json("server is on")
+})
+app.post('/', (req,res) => {
+    res.json(req.body)
+    knex('example_db_table')
+        .insert({name: req.body.name, password: req.body.password, email: req.body.email})
+        .then(() =>{
+            console.log("Data has been inserted successfully.")
+        })
 })
 
 app.listen(port,() => {
